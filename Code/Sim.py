3e71c8ae127327a28a91b112b1044e08175d30f1
FieryRaccoon23 <araib.saleem@gmail.com>
2025-09-04 17:02:16 -0400

    Moved the code to particle merging splitting project

--- Code/Sim.py
+++ Code/Sim.py
@@ -1,67 +1,33 @@
-from Shapes import SHAPES, Shape, Circle, Rectangle
-import random
 import pygame
-import DebugShapes
+from enum import Enum
+import ParticleMergingSplitting
 
-GRAVITY = 9.81        
-RESTITUTION = 0.5     # 0 = no bounce, 1 = perfectly bouncy
-PHYSICS_OBJECTS: list["Shape"] = []
-RADIUS = 10
+class Project(Enum):
+    ParticleMergingSplitting = 1
 
-Floor = None
-Window = None
+SELECTED_PROJECT = Project.ParticleMergingSplitting
 
 # Called once
 def sim_init(window: pygame.Surface):
-    global Window
-    Window = window
-
-    # Init circles
-    count = 10
-    x_value = 50
-    x_offset = 60
-    object_id = 0
-    for _ in range(count):
-        x = x_value
-        x_value += (2* RADIUS) + x_offset
-        y = random.randint(50, 100)
-
-        circle_mass = 1.0
-        c = Circle(x, y, RADIUS, circle_mass, object_id, (255, 0, 0))
-        object_id += 1
-        c.add_shape()
-        c.text_color = (255, 0, 0)            
-        PHYSICS_OBJECTS.append(c)
-    
-
-    # Init floor
-    global Floor
-    floor_mass = 1.0
-    Floor = Rectangle(300, 500, 300, 10, floor_mass, object_id, [100, 200, 200])
-    Floor.static_object = True
-    Floor.text_color = (100, 200, 200)
-    Floor.angle = 0.0
-    Floor.add_shape()
-    PHYSICS_OBJECTS.append(Floor)
-
-# Debug
-def show_debug(object: Shape):
-    object.set_text(f"id: {object.id}, vx: {object.vx:.1f}, vy: {object.vy:.1f}", RADIUS, RADIUS, object.text_color)
-
-# Gravity
-def apply_gravity(dt: int, object: Shape):
-    if not object.static_object:
-        object.vy += GRAVITY * dt
-        object.y += object.vy * dt
+    match SELECTED_PROJECT:
+        case Project.ParticleMergingSplitting:
+            print("Running ParticleMergingSplitting")
+            ParticleMergingSplitting.init(window)
+        case _:
+            print("No project selected")
 
 # Updates every frame
 def sim_loop(dt_ms: int):
-    dt = dt_ms / 1000.0
-
-    for obj in PHYSICS_OBJECTS:
-        apply_gravity(dt, obj)
-        show_debug(obj)
+    match SELECTED_PROJECT:
+        case Project.ParticleMergingSplitting:
+            ParticleMergingSplitting.loop(dt_ms)
+        case _:
+            pass
 
 # Update every frame for debug displaying
 def sim_debug_display():
-    pass
+    match SELECTED_PROJECT:
+        case Project.ParticleMergingSplitting:
+            ParticleMergingSplitting.debug_display()
+        case _:
+            pass
